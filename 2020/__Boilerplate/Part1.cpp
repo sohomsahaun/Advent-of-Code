e665914ef4f3dd2fdd8c0caa018a1008bca94ad9
Sohom Sahaun <ssahaun19@gmail.com>
2020-12-03 10:27:05 +0600

    Boilerplate

---
+++ 2020/__Boilerplate/Part1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int solve(vector<int>& v) {
+
+}
+
+int main() {
+    freopen("in1.txt", "r", stdin);
+
+    int n;
+    vector<int> input;
+    while (cin >> n) {
+        input.push_back(n);
+    }
+
+    cout << solve(input);
+
+    return 0;
+}
