90d6d0a0c5344cbf979b18e0aa5da23efca52bc5
Sohom Sahaun <ssahaun19@gmail.com>
2023-12-02 11:17:38 +0600

    Day 2

---
+++ 2023/Day 2 - Cube Conundrum/Solution_P1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+map<ll, vector<tuple<ll,ll,ll>>> mp;
+
+void get_input() {
+  string s;
+  while (getline(cin, s)) {
+    s += ";";
+    stringstream ss(s);
+    string _, id, col;
+    ss >> _;
+    ss >> id; id.pop_back();
+    ll cnt, r, g, b;
+    r = g = b = 0;
+    while (ss >> cnt >> col) {
+      bool f = (col.back() == ';');
+      col.pop_back();
+      if (col[0] == 'r') r += cnt;
+        else if (col[0] == 'g') g += cnt;
+        else b += cnt;
+      if (f) {
+        mp[stoll(id)].push_back(make_tuple(r, g, b));
+        r = g = b = 0;
+      }
+    }
+  }
+}
+
+ll solve() {
+  ll ans = 0;
+  for (auto [id, a] : mp) {
+    bool f = 1;
+    for (auto [r, g, b] : a) {
+      if ((r > 12) || (g > 13) || (b > 14)) f = 0;
+    }
+    if (f) ans += id;
+  }
+  return ans;
+}
+
+int main() {
+  freopen("in.txt", "r", stdin);
+
+  get_input();
+  cout << solve() << "\n";
+
+  return 0;
+}
